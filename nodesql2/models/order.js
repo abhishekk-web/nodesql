1e5dd783edff8a493ed3bee49e9e78c154e39a8b
abhishekk-web <abhi.chatterjee38@gmail.com>
2022-12-05 11:15:57 -0800

    now ordering is showing with the complete scroll bar in cart

--- nodesql2/models/order.js
+++ nodesql2/models/order.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Order = sequelize.define('Order',{
+const Order = sequelize.define('order',{
 
   id: {
     type: Sequelize.INTEGER,
